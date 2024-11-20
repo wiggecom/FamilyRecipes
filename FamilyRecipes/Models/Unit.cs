db75d0d126dd8fd794b2b8b0f390f1caf9d08e98
Mattias Wigren <wigge@wigge.com>
2024-11-21 00:14:03 +0100

    Created Unit.cs

---
+++ FamilyRecipes/Models/Unit.cs
@@ -0,0 +1,25 @@
+﻿namespace FamilyRecipes.Models
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsMetrical { get; set; }
+
+        public Unit()
+        {
+
+        }
+        public static List<Unit> GetUnits()
+        {
+            return new List<Unit>
+            {
+                new Unit { IsMetrical = true, Name = "Litre(s)"},
+                new Unit { IsMetrical = true, Name = "Decilitre(s)"},
+                new Unit { IsMetrical = false, Name = "Ounce(s)"},
+                new Unit { IsMetrical = false, Name = "Quart(s)"},
+                new Unit { IsMetrical = false, Name = "Gallon(s)"}
+            };
+        }
+    }
+}
