edebb540277a99223db7c8ecb8a21382f21d4794
Mattias Wigren <wigge@wigge.com>
2024-11-21 14:32:54 +0100

    Updated Recipe
    Changed placeholder to properties

--- FamilyRecipes/Models/Recipe.cs
+++ FamilyRecipes/Models/Recipe.cs
@@ -2,8 +2,22 @@
 {
     public class Recipe
     {
-        // Title, Description, <RecipeIngredient>, <UserRating>, Category,
-        // Image, TimeRequired, DateTime, AgeCheck
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Category { get; set; }
+        public int TimeRequired { get; set; }
+        public string Description { get; set; }
+        public List<string> Steps { get; set; }
+        public List<RecipeIngredient> Ingredients { get; set; } = new List<RecipeIngredient>();
+        public List<UserRating> UserRatings { get; set; } = new List<UserRating>();
+        public int AgeCheck { get; set; }
+        public string Image { get; set; }
 
+        public Recipe()
+        {
+            
+        }
     }
 }
