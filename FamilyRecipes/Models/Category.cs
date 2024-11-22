99162f9d012371c536a3179546d8561d8b6a7ccc
Mattias Wigren <wigge@wigge.com>
2024-11-22 15:49:48 +0100

    Create Category.cs

---
+++ FamilyRecipes/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace FamilyRecipes.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryType { get; set; }
+        public string CategoryName { get; set; }
+
+        public Category()
+        {
+            
+        }
+    }
+}
