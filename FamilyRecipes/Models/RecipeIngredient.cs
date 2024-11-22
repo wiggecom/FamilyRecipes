405aa593f45147ba556192a633e70b916f306607
Mattias Wigren <wigge@wigge.com>
2024-11-22 22:57:37 +0100

    Update RecipeIngredient.cs
    Changed Unit of type string to Unit of type Unit.
    Reworked formula for calculating calories to cast to float while calculating, then rounding back to int before returning

--- FamilyRecipes/Models/RecipeIngredient.cs
+++ FamilyRecipes/Models/RecipeIngredient.cs
@@ -4,8 +4,11 @@
     {
         // RecipeIngredient: [Ingredient], Unit, Amount, Calories
         public int Id { get; set; }
+        public int RecipeId { get; set; }
         public Ingredient Ingredient { get; set; }
-        public string Unit { get; set; }
+        public int IngredientId { get; set; }
+        public Unit Unit { get; set; }
+        public int UnitId { get; set; }
         public bool IsMetrical { get; set; }
         public int Amount { get; set; }
         public int TotalCalories { get; set; }
@@ -17,7 +20,14 @@
 
         public static int CalculateTotalCalories(int calcAmount, int calcIngredientCalories)
         {
-            if (calcIngredientCalories > 0) return (calcIngredientCalories / 100) * calcAmount; // Early return
+            float ingredientCaloriesFloat = (float)calcIngredientCalories/100;
+            //if (calcIngredientCalories > 0) return (int)Math.Round((ingredientCaloriesFloat * calcAmount),0); // Early return
+            if (calcIngredientCalories > 0) 
+            { 
+                int myNum = (int)Math.Round((ingredientCaloriesFloat * calcAmount), 0); // Early return
+
+                return myNum;
+            }
             return 0;
         }
     }
