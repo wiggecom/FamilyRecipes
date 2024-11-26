df85f54f8231e5107c9dd93db6697fc7e66dc155
Mattias Wigren <wigge@wigge.com>
2024-11-26 14:45:01 +0100

    Update Category.cs
    Renamed props for clarity

--- FamilyRecipes/Models/Category.cs
+++ FamilyRecipes/Models/Category.cs
@@ -3,8 +3,8 @@
     public class Category
     {
         public int Id { get; set; }
-        public string CategoryType { get; set; }
-        public string CategoryName { get; set; }
+        public string MainCategory { get; set; }
+        public string SubCategory { get; set; }
 
         public Category()
         {
