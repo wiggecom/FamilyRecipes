d217de12b88a8d49a2f125aa0efd6efd761396a8
Mattias Wigren <wigge@wigge.com>
2024-11-28 09:51:42 +0100

    Update FamilyUser.cs

--- FamilyRecipes/Models/FamilyUser.cs
+++ FamilyRecipes/Models/FamilyUser.cs
@@ -4,12 +4,15 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Password { get; set; }
+        public string Password { get; set; } // can be reset by IsAdmin
         public bool IsLoggedIn { get; set; }
-        public bool IsAdmin { get; set; }
-        public bool IsAdult { get; set; }
+        public bool IsAdmin { get; set; } // first user is IsAdmin by default
+        public bool IsAdult { get; set; } // can only be set by IsAdmin
         public string UserImage { get; set; } // Image
 
+        //TODO: Add form to add or change users,
+        //if FamilyUser.Any() > require password for form
+        //else show form
 
         public FamilyUser()
         {
