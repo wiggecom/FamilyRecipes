6135fe712362eeab0cc3d7df441e619b40e41df5
Mattias Wigren <wigge@wigge.com>
2024-11-29 14:30:24 +0100

    Changed Ingredient property name
    IsMeasuredAsFluid is now IsMeasuredByVolume

--- FamilyRecipes/Models/Ingredient.cs
+++ FamilyRecipes/Models/Ingredient.cs
@@ -8,7 +8,50 @@
         public string Description { get; set; }
         public string Type { get; set; }
         public int Calories { get; set; } // per 100g
-        public bool IsMeasuredAsFluid { get; set; } // Helps selecting available measures
+        public bool IsMeasuredByVolume { get; set; } // Helps selecting available measures
         public Ingredient() { }
+
+        public static List<string> GetIngredientTypes()
+        {
+            var ingredientTypes = new List<string> {
+                // Animal products
+                "Meat",
+                "Poultry",
+                "Fish", 
+                "Seafood",
+                "Egg",
+                "Dairy",
+                // Grown products
+                "Nut/Seed",
+                "Bean",
+                "Fruit/Berry",
+                "Vegetable",
+                "Wheat",
+                "Legume",
+                "Grain",
+                // Misc
+                "Snack",
+                "Beverage",
+                "Condiment",
+                "Alcohol",
+                "Other",
+                // Basics
+                "Salt",
+                "Sugar",
+                "Water",
+                "Oil",
+                "Condiment",
+                // Additives and Spices
+                "Herb",
+                "Bullion",
+                "Chemical",
+                "Coloring",
+                "Flavor",
+                "Essential Oil",
+                "Extract",
+                "Concentrate"
+            };
+            return ingredientTypes;
+        }
     }
 }
