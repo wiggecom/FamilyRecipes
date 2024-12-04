5012967a3df28fd89b802c16ec6b20bf3d1096ad
Mattias Wigren <wigge@wigge.com>
2024-12-04 09:59:00 +0100

    Create RecipeIngredientMapSource.cs

---
+++ FamilyRecipes/Models/RecipeIngredientMapSource.cs
@@ -0,0 +1,9 @@
+﻿namespace FamilyRecipes.Models
+{
+    public class RecipeIngredientMapSource
+    {
+            public string IngredientName { get; set; }
+            public string UnitName { get; set; }
+            public int Amount { get; set; }
+    }
+}
