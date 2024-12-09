49526cde9514b94d4d30e4abf18f8bc41e6ed7bc
Mattias Wigren <wigge@wigge.com>
2024-12-09 18:01:06 +0100

    Electricity Prices
    Just a small feature to show electricity rate, to aid in picking time to cook food.

---
+++ FamilyRecipes/Helpers/Electricity.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using FamilyRecipes.Models;
+using System.Diagnostics;
+
+namespace FamilyRecipes.Helpers
+{
+    public class Electricity
+    {
+        private static Uri BaseAddress = new Uri("https://www.elprisetjustnu.se");
+        public Electricity()
+        {
+            
+        }
+
+        public static async Task<List<ElectricityPrice>> GetPrice(int dayAdd)
+        {
+            List<ElectricityPrice> priceList = new List<ElectricityPrice>();
+
+            string baseurl = "https://www.elprisetjustnu.se";
+            string year = DateTime.Now.ToString("yyyy");
+            string month = DateTime.Now.ToString("MM");
+            string day = DateTime.Now.AddDays(dayAdd).ToString("dd");
+            string region = "SE3";
+            string apiurl = "/api/v1/prices/" + year + "/" + month + "-" + day + "_" + region + ".json";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = BaseAddress;
+                HttpResponseMessage response = await client.GetAsync(apiurl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    priceList = JsonSerializer.Deserialize<List<ElectricityPrice>>(responseString);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("No bueno hombre!");
+                }
+                return priceList;
+            }
+        }
+    }
+
+
+}
---
+++ FamilyRecipes/Models/ElectricityPrice.cs
@@ -0,0 +1,16 @@
+﻿namespace FamilyRecipes.Models
+{
+    public class ElectricityPrice
+    {
+        public double SEK_per_kWh {  get; set; }
+        public double EUR_per_kWh { get; set; }
+        public double EXR { get; set; }
+        public DateTime time_start { get; set; }
+        public DateTime time_end { get; set; }
+
+        public ElectricityPrice()
+        {
+            
+        }
+    }
+}
