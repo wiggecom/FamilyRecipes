e49d844b34ea06da138a7991657ec807e946f060
Mattias Wigren <wigge@wigge.com>
2024-12-10 10:55:06 +0100

    Migration properties changed

---
+++ FamilyRecipes/Data/Migrations/20241210031757_refined props ingredient and category.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyRecipes.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class refinedpropsingredientandcategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryIcon",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryImage",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypicalUnitWeight",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Volume100Grams",
+                table: "Ingredients",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypicalUnitWeight",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "Volume100Grams",
+                table: "Ingredients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryIcon",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryImage",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
