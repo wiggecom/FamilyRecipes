0c9766545c3232707abcd46db797d6758b053a23
Mattias Wigren <wigge@wigge.com>
2024-12-10 10:57:11 +0100

    Recipes-page
    Started work on recipe selector

--- FamilyRecipes/Pages/Recipes.cshtml.cs
+++ FamilyRecipes/Pages/Recipes.cshtml.cs
@@ -17,30 +17,25 @@ namespace FamilyRecipes.Pages
             this.webHostEnvironment = webHostEnvironment;
         }
 
-        public List<Unit> Units { get; set; } = Models.Unit.GetUnits();
+        public List<Unit> Units { get; set; } = Unit.GetUnits();
         public List<Category> Categories { get; set; } = new List<Category>();
         public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
         public Recipe MyRecipe { get; set; } = new Recipe();
         public bool HasCaloriesAny { get; set; } = false;
 
 
-        public string success = "Boo!";
-        public async void OnGet()
+        public void OnGet()
         {
-            foreach (var unit in Units)
-            {
-                System.Diagnostics.Debug.Write(unit.Name);
-                if (unit.IsMetrical) System.Diagnostics.Debug.WriteLine(" is a metrical measure");
-                if (!unit.IsMetrical) System.Diagnostics.Debug.WriteLine(" is an imperial measure");
-            }
-
             Categories = _context.Categories.ToList();
+            Ingredients = _context.Ingredients.ToList();
+
             Recipe? firstRecipe = _context.Recipes
                     .Include(r => r.RecipeIngredients)
                     .ThenInclude(ri => ri.Ingredient) // Include Ingredient
                     .Include(r => r.RecipeIngredients)
                     .ThenInclude(ri => ri.Unit)      // Include Unit
                     .FirstOrDefault();
+
             if (firstRecipe != null)
             {
                 MyRecipe = firstRecipe;
@@ -49,13 +44,43 @@ namespace FamilyRecipes.Pages
                     if(ri.TotalCalories > 0) { HasCaloriesAny = true; break; }
                 }
             }
+        }
 
-            Ingredients = _context.Ingredients.ToList();
-            if (MyRecipe != null)
-            {
-                Thread.Sleep(1000);
-            }
+        public JsonResult OnGetGetSubcategories(string mainCategory)
+        {
+            // Use _context to fetch subcategories for the selected main category
+            var subcategories = _context.Categories
+                .Where(c => c.MainCategory == mainCategory)
+                .Select(c => c.SubCategory)
+                .ToList();
 
+            return new JsonResult(subcategories);
+        }
+
+        public JsonResult OnGetGetRecipes(string subCategory)
+        {
+            // Use _context to fetch subcategories for the selected main category
+            var recipes = _context.Recipes
+                .Include(c => c.Category)
+                .Where(c => c.Category.SubCategory == subCategory)
+                .Select(c => c.Title)
+                .ToList();
+
+            return new JsonResult(recipes);
+        }
+
+        public JsonResult OnGetGetSelectRecipe(string selectRecipe)
+        {
+            // Use _context to fetch subcategories for the selected main category
+            var recipe = _context.Recipes
+                .Include(c => c.Category)
+                .Include(c => c.RecipeIngredients)
+                .ThenInclude(c => c.Ingredient)
+                .Include(c => c.RecipeIngredients)
+                .ThenInclude(c => c.Unit)
+                .FirstOrDefault();
+
+            return new JsonResult(recipe);
         }
     }
 }
